cbec822f1d63c3f029c7a3befc74381a354d5a2f
Abdullah AKALIN <abdullah.akalin@technarts.com>
2024-09-21 21:19:33 +0300

    Something exciting.

---
+++ Font Fountain/src/main.js
@@ -0,0 +1,2 @@
+const app = document.getElementById("App");
+app.innerHTML = "<p>“The purpose of abstracting is not to be vague, but to create a new semantic level in which one can be absolutely precise.” ― Edsger W. Dijkstra</p>"
